0aaec38066d1b865ce68492c741e025a90c4cfc5
hytan50 <hytan50@gmail.com>
2018-04-24 20:09:28 +1000

    Automation tests are created * functions.js is the file that should contains all math functions included in calculator app * testfunctions.js is the file that contains all automation test for the functions in functions.js * unitTest_calculator.htm is used to display the results of the automation test in testfunctions.js

---
+++ functions.js
@@ -0,0 +1,15 @@
+function addition(x, y){
+
+}
+
+function subtraction(x, y){
+
+}
+
+function multiplication(x, y){
+
+}
+
+function division(x, y){
+
+}
---
+++ testfunctions.js
@@ -0,0 +1,54 @@
+
+const mathFunctions = document.createElement('script');
+mathFunctions.src = 'functions.js';
+document.head.appendChild(mathFunctions);
+
+//unit test for function "addition"
+function testAddition(){
+
+    if (addition(2, 3) !== 5){
+        //throw new Error("Addition of 2 and 3 gives 5, but the addition function gives " + addition(2, 3));
+        document.getElementById("testAdditionResult").innerHTML = "Addition of 2 and 3 gives 5, but the addition function gives " + addition(2, 3);
+    }
+    else{
+        document.getElementById("testAdditionResult").innerHTML = "test passed";
+    }
+}
+
+//unit test for function "subtraction"
+function testSubtraction(){
+    if (subtraction(5, 2) !== 3){
+        //throw new Error("Subtraction of 2 from 5 gives 3, but the subtraction function gives " + subtraction(5, 2));
+        document.getElementById("testSubtractionResult").innerHTML = "Subtraction of 2 from 5 gives 3, but the subtraction function gives " + subtraction(5, 2);
+    }
+    else{
+        document.getElementById("testSubtractionResult").innerHTML = "test passed";
+    }
+}
+
+//unit test for function "multiplication"
+function testMultiplication(){
+    if (multiplication(2, 3) !== 6){
+        //throw new Error("Multiplication of 2 and 3 gives 6, but the multiplication function gives " + multiplication(2, 3));
+        document.getElementById("testMultiplicationResult").innerHTML = "Multiplication of 2 by 3 gives 6, but the multiplication function gives " + multiplication(2, 3);
+    }
+    else{
+        document.getElementById("testMultiplicationResult").innerHTML = "test passed";
+    }
+}
+
+//unit test for function "division"
+function testDivision(){
+    if (division(10, 2) !== 5){
+        //throw new Error("10 divided by 2 gives 5, but the division function gives "+ division(10, 2));
+        document.getElementById("testDivisionResult").innerHTML = "10 divided by 2 gives 5, but the division function gives "+ division(10, 2);
+    }
+    else{
+        document.getElementById("testDivisionResult").innerHTML = "test passed";
+    }
+}
+
+testAddition();
+testSubtraction();
+testMultiplication();
+testDivision();
