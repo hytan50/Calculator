9a58b092ef63377c73ad7dc4e8a858a2262ebbb5
hytan50 <hytan50@gmail.com>
2018-05-08 22:14:20 +1000

    Group the tests into different groups

--- js/unitTest_calculator.js
+++ js/unitTest_calculator.js
@@ -42,132 +42,142 @@ describe('Controller', function () {    //describe object type
                 expect($scope.errorMessage).toEqual("Please enter valid numbers into the textbox given.");
             });
 
-            it('addition of 3 and 2 should be 5', function(){
-                $scope.firstNumber = 3;
-                $scope.secondNumber = 2;
-                $scope.operation = "+";
-                $scope.updateResult();
-                expect($scope.result).toEqual(5);
-                expect($scope.errorMessage).toEqual("");
+            describe('Addition function', function(){
+                it('Addition of 3 and 2 should be 5', function(){
+                    $scope.firstNumber = 3;
+                    $scope.secondNumber = 2;
+                    $scope.operation = "+";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(5);
+                    expect($scope.errorMessage).toEqual("");
+                });
             });
 
-            it('subtraction of 3 from 5 should be 2', function(){
-                $scope.firstNumber = 5;
-                $scope.secondNumber = 3;
-                $scope.operation = "-";
-                $scope.updateResult();
-                expect($scope.result).toEqual(2);
-                expect($scope.errorMessage).toEqual("");
+            describe('Subtraction function', function(){
+                it('Subtraction of 3 from 5 should be 2', function(){
+                    $scope.firstNumber = 5;
+                    $scope.secondNumber = 3;
+                    $scope.operation = "-";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(2);
+                    expect($scope.errorMessage).toEqual("");
+                });
             });
 
-            it('multiplication of 3 and 2 should be 6', function(){
-                $scope.firstNumber = 3;
-                $scope.secondNumber = 2;
-                $scope.operation = "*";
-                $scope.updateResult();
-                expect($scope.result).toEqual(6);
-                expect($scope.errorMessage).toEqual("");
+            describe('Multiplication function', function(){
+                it('Multiplication of 3 and 2 should be 6', function(){
+                    $scope.firstNumber = 3;
+                    $scope.secondNumber = 2;
+                    $scope.operation = "*";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(6);
+                    expect($scope.errorMessage).toEqual("");
+                });
             });
 
-            it('6 divide by 2 should be 3', function (){
-                $scope.firstNumber = 6;
-                $scope.secondNumber = 2;
-                $scope.operation = "/";
-                $scope.updateResult();
-                expect($scope.result).toEqual(3);
-                expect($scope.errorMessage).toEqual("");
+            describe('Division function', function(){
+                it('6 divide by 2 should be 3', function (){
+                    $scope.firstNumber = 6;
+                    $scope.secondNumber = 2;
+                    $scope.operation = "/";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(3);
+                    expect($scope.errorMessage).toEqual("");
+                });
+
+                it("Division of 0 should be error", function(){
+                    $scope.firstNumber = 3;
+                    $scope.secondNumber = 0;
+                    $scope.operation = "/";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual("");
+                    expect($scope.errorMessage).toEqual("Division by 0 is undefined. Please enter a non-zero value for the second number.");
+                });
             });
 
-            it("Division of 0 should be error", function(){
-                $scope.firstNumber = 3;
-                $scope.secondNumber = 0;
-                $scope.operation = "/";
-                $scope.updateResult();
-                expect($scope.result).toEqual("");
-                expect($scope.errorMessage).toEqual("Division by 0 is undefined. Please enter a non-zero value for the second number.");
+            describe('Square By function', function(){
+                it('3 square by 2 should be 9', function(){
+                    $scope.firstNumber = 3;
+                    $scope.secondNumber = 2;
+                    $scope.operation = "^";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(9);
+                    expect($scope.errorMessage).toEqual("");
+                });
+
+                it('any number square by 0 should be 1', function(){
+                    $scope.firstNumber = 3;
+                    $scope.secondNumber = 0;
+                    $scope.operation = "^";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(1);
+                    expect($scope.errorMessage).toEqual("");
+                });
+
+                it('2 square by -2 should be 0.25', function(){
+                    $scope.firstNumber = 2;
+                    $scope.secondNumber = -2;
+                    $scope.operation = "^";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(0.25);
+                    expect($scope.errorMessage).toEqual("");
+                });
             });
 
-            it('3 square by 2 should be 9', function(){
-                $scope.firstNumber = 3;
-                $scope.secondNumber = 2;
-                $scope.operation = "^";
-                $scope.updateResult();
-                expect($scope.result).toEqual(9);
-                expect($scope.errorMessage).toEqual("");
-            });
+            describe('Square root function', function(){
+                it('square root 0 should be 0', function(){
+                    $scope.secondNumber = 0;
+                    $scope.operation = "√";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(0);
+                    expect($scope.errorMessage).toEqual("");
+                });
 
-            it('any number square by 0 should be 1', function(){
-                $scope.firstNumber = 3;
-                $scope.secondNumber = 0;
-                $scope.operation = "^";
-                $scope.updateResult();
-                expect($scope.result).toEqual(1);
-                expect($scope.errorMessage).toEqual("");
-            });
+                it('square root 9 should be 3', function(){
+                    $scope.firstNumber = 1;
+                    $scope.secondNumber = 9;
+                    $scope.operation = "√";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(3);
+                    expect($scope.errorMessage).toEqual("");
+                });
 
-            it('2 square by -2 should be 0.25', function(){
-                $scope.firstNumber = 2;
-                $scope.secondNumber = -2;
-                $scope.operation = "^";
-                $scope.updateResult();
-                expect($scope.result).toEqual(0.25);
-                expect($scope.errorMessage).toEqual("");
-            });
+                it('square root 2 should be 1.414', function(){
+                    $scope.firstNumber = 1;
+                    $scope.secondNumber = 2;
+                    $scope.operation = "√";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(1.414);
+                    expect($scope.errorMessage).toEqual("");
+                });
 
-            it('square root 0 should be 0', function(){
-                $scope.secondNumber = 0;
-                $scope.operation = "√";
-                $scope.updateResult();
-                expect($scope.result).toEqual(0);
-                expect($scope.errorMessage).toEqual("");
-            });
+                it('2 * square root 9 should be 6', function(){
+                    $scope.firstNumber = 2;
+                    $scope.secondNumber = 9;
+                    $scope.operation = "√";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(6);
+                    expect($scope.errorMessage).toEqual("");
+                });
 
-            it('square root 9 should be 3', function(){
-                $scope.firstNumber = 1;
-                $scope.secondNumber = 9;
-                $scope.operation = "√";
-                $scope.updateResult();
-                expect($scope.result).toEqual(3);
-                expect($scope.errorMessage).toEqual("");
-            });
+                it('2 * square root 2 should be 2.828', function(){
+                    $scope.firstNumber = 2;
+                    $scope.secondNumber = 2;
+                    $scope.operation = "√";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual(2.828);
+                    expect($scope.errorMessage).toEqual("");
+                });
 
-            it('square root 2 should be 1.414', function(){
-                $scope.firstNumber = 1;
-                $scope.secondNumber = 2;
-                $scope.operation = "√";
-                $scope.updateResult();
-                expect($scope.result).toEqual(1.414);
-                expect($scope.errorMessage).toEqual("");
+                it('square root negative number should be error', function(){
+                    $scope.firstNumber = 1;
+                    $scope.secondNumber = -1;
+                    $scope.operation = "√";
+                    $scope.updateResult();
+                    expect($scope.result).toEqual("");
+                    expect($scope.errorMessage).toEqual("Negative number cannot be square rooted. Please enter a positive number to the second number.");
+                });
             });
-
-            it('2 * square root 9 should be 6', function(){
-                $scope.firstNumber = 2;
-                $scope.secondNumber = 9;
-                $scope.operation = "√";
-                $scope.updateResult();
-                expect($scope.result).toEqual(6);
-                expect($scope.errorMessage).toEqual("");
-            });
-
-            it('2 * square root 2 should be 2.828', function(){
-                $scope.firstNumber = 2;
-                $scope.secondNumber = 2;
-                $scope.operation = "√";
-                $scope.updateResult();
-                expect($scope.result).toEqual(2.828);
-                expect($scope.errorMessage).toEqual("");
-            });
-
-            it('square root negative number should be error', function(){
-                $scope.firstNumber = 1;
-                $scope.secondNumber = -1;
-                $scope.operation = "√";
-                $scope.updateResult();
-                expect($scope.result).toEqual("");
-                expect($scope.errorMessage).toEqual("Negative number cannot be square rooted. Please enter a positive number to the second number.");
-            });
-
         });
-
     });
 });
